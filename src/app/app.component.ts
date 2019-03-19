58d2771799c67926b70916df0b44092b231e0d3d
Roque Tromp <rtromp.007@gmail.com>
2019-03-19 20:02:34 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rabo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'rabo-assignment';
+}
